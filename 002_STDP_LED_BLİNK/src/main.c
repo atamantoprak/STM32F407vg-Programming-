bee2b5245abffc3482fa83c87b8ccce970c24514
Emir Toprak Ataman <75549605+atamantoprak@users.noreply.github.com>
2023-04-01 00:38:10 +0300

    led blik with stdperiph
    we write this code using standard peripheral library

--- 002_STDP_LED_BLİNK/src/main.c
+++ 002_STDP_LED_BLİNK/src/main.c
@@ -4,12 +4,12 @@
 
 	GPIO_InitTypeDef GPIO_InitStructure ;
 
-// this function makes pins configure 
+// this function makes pins configure
 void GPIO_Config(){
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD , ENABLE); //GPIOD CLK ENABLE
 
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT; 
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_NOPULL;
