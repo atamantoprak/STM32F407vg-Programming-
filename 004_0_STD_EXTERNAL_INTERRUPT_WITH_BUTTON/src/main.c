6e2e899f8066a235208f8eefef0ba7fcbf53ff43
Emir Toprak Ataman <75549605+atamantoprak@users.noreply.github.com>
2023-04-21 17:32:43 +0300

    external interrupt
    INTERRUPT with std periph library

---
+++ 004_0_STD_EXTERNAL_INTERRUPT_WITH_BUTTON/src/main.c
@@ -0,0 +1,90 @@
+
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+GPIO_InitTypeDef GPIO_InitStruct ;
+EXTI_InitTypeDef EXTI_InitStruct ;
+NVIC_InitTypeDef NVIC_InitStruct ;
+
+void delay(uint32_t time){
+	while(time--);
+}
+
+void GPIO_Config(){
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG,ENABLE); // EXTI clock birimini aktive etme
+
+	GPIO_InitStruct.GPIO_Mode=GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType=GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Pin=GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStruct.GPIO_PuPd=GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Speed=GPIO_Speed_100MHz;
+
+	GPIO_Init(GPIOD,&GPIO_InitStruct);
+
+	GPIO_InitStruct.GPIO_Mode=GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_OType=GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Pin=GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_PuPd=GPIO_PuPd_DOWN;
+	GPIO_InitStruct.GPIO_Speed=GPIO_Speed_100MHz;
+
+	GPIO_Init(GPIOA,&GPIO_InitStruct);
+
+}
+
+void EXTI_Config(){
+
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA,EXTI_PinSource0);
+
+	EXTI_InitStruct.EXTI_Line=EXTI_Line0;
+	EXTI_InitStruct.EXTI_LineCmd=ENABLE;
+	EXTI_InitStruct.EXTI_Mode=EXTI_Mode_Interrupt;
+	EXTI_InitStruct.EXTI_Trigger=EXTI_Trigger_Rising;  // yükselen kenarda kesme iþlemi yapma tanýmý
+
+	EXTI_Init(&EXTI_InitStruct);
+
+	NVIC_InitStruct.NVIC_IRQChannel=EXTI0_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority=0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority=0;
+
+	NVIC_Init(&NVIC_InitStruct);
+}
+
+// kesme iþlemi gerçekleþtiðinde yappýlacak iþlemler bu fonksiyona yazýlýr
+void EXTI0_IRQHandler(){
+
+	if(EXTI_GetITStatus(EXTI_Line0)!=RESET){  // if EXTI_GetITStatus fonksiyonu belirtilen line'da deðiþim var mý kontrol eder
+		GPIO_ResetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+		delay(1680000);
+		GPIO_SetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+		delay(1680000);
+		GPIO_ResetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+		delay(1680000);
+
+		EXTI_ClearITPendingBit(EXTI_Line0); // kesme iþlemini tekrar kullanabilmek için bayrak 0a çekilir
+	}
+}
+
+int main(void)
+{
+	GPIO_Config();
+	EXTI_Config();
+  while (1)
+  {
+	GPIO_SetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+  }
+}
+
+
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
